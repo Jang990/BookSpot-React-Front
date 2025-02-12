3c072d3694f633459e0fb62c7672faf248c9c0d7
Jang <sdsd090811@gmail.com>
2025-02-12 18:21:24 +0900

    feat: 무한 스크롤 기능 추가

--- components/organisms/BookSearchPreview.tsx
+++ components/organisms/BookSearchPreview.tsx
@@ -1,14 +1,17 @@
+import { Loader } from "lucide-react";
 import BookInfo from "../Card/BookInfo";
 import { EmptySearchResult } from "../molecules/EmptySearchResult";
 import { BookPreview } from "@/types/BookPreview";
 
 interface SimpleSearchResultProps {
   searchResults: BookPreview[];
+  isLoading: boolean;
   lastBookElementRef: (node: HTMLDivElement | null) => void;
 }
 
 export const BookSimpleSearchResult = ({
   searchResults,
+  isLoading,
   lastBookElementRef,
 }: SimpleSearchResultProps) => {
   return (
@@ -16,18 +19,25 @@ export const BookSimpleSearchResult = ({
       {searchResults.length === 0 ? (
         <EmptySearchResult />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {searchResults.map((book, index) => (
-            <div
-              key={book.id}
-              ref={
-                index === searchResults.length - 1 ? lastBookElementRef : null
-              } // 무한 스크롤용
-            >
-              <BookInfo book={book} isInCart={false} />
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {searchResults.map((book, index) => (
+              <div
+                key={book.id}
+                ref={
+                  index === searchResults.length - 1 ? lastBookElementRef : null
+                } // 무한 스크롤용
+              >
+                <BookInfo book={book} isInCart={false} />
+              </div>
+            ))}
+          </div>
+          {isLoading && (
+            <div className="flex justify-center items-center mt-8">
+              <Loader className="animate-spin text-primary" size={32} />
             </div>
-          ))}
-        </div>
+          )}
+        </>
       )}
     </>
   );
--- components/templates/BookSearchPreviewTemplate.tsx
+++ components/templates/BookSearchPreviewTemplate.tsx
@@ -14,42 +14,92 @@ interface BookSearchProps {
 
 const ITEMS_PER_PAGE = 12;
 const MIN_SEARCH_TERM_LENGTH = 2;
+const FIRST_PAGE = 0;
 
 export default function BookSearch({ lastBookElementRef }: BookSearchProps) {
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState<BookPreview[]>([]);
 
-  const debouncedSearch = useCallback(
-    debounce(async (term: string, page: number) => {
+  const loadBooks = useCallback(
+    async (term: string, currentPage: number) => {
+      setIsLoading(true);
       try {
         const json = await fetchBooksPreview({
           keyword: term,
-          pageNumber: page,
+          pageNumber: currentPage,
           pageSize: ITEMS_PER_PAGE,
         });
-        setSearchResults(json.content.map(convertBookPreview));
+
         setHasMore(!json.last);
+        const newBooks = json.content.map(convertBookPreview);
+        console.log(newBooks);
+        if (currentPage === FIRST_PAGE) {
+          setSearchResults(newBooks);
+        } else {
+          setSearchResults((prev) => [...prev, ...newBooks]);
+        }
       } catch (error) {
         // TODO: 예외 처리 필요
-        console.error(error);
+        console.error("Error fetching books:", error);
+      } finally {
+        setIsLoading(false);
       }
-    }, 500),
-    []
+    },
+    [setIsLoading, setHasMore]
+  );
+
+  const debouncedSearch = useCallback(
+    debounce((term: string) => {
+      setPage(FIRST_PAGE);
+      loadBooks(term, FIRST_PAGE);
+    }, 300),
+    [loadBooks]
   );
 
   useEffect(() => {
-    if (searchTerm.length < MIN_SEARCH_TERM_LENGTH) return;
+    if (searchTerm && searchTerm.length >= MIN_SEARCH_TERM_LENGTH) {
+      debouncedSearch(searchTerm);
+    } else {
+      setPage(FIRST_PAGE);
+      loadBooks("", FIRST_PAGE);
+    }
+  }, [searchTerm, debouncedSearch, setPage, loadBooks]);
 
-    debouncedSearch(searchTerm, page);
-  }, [searchTerm, debouncedSearch, page, setHasMore]);
+  useEffect(() => {
+    if (page > FIRST_PAGE) {
+      loadBooks(searchTerm, page);
+    }
+  }, [page, searchTerm, loadBooks]);
+
+  const handleLoadMore = useCallback(() => {
+    if (!isLoading && hasMore) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  }, [isLoading, hasMore, setPage]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.offsetHeight - 100
+      ) {
+        handleLoadMore();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleLoadMore]);
 
   return (
     <div className="w-full max-w-4xl mx-auto">
       <BookSearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <BookSimpleSearchResult
         searchResults={searchResults}
+        isLoading={isLoading}
         lastBookElementRef={lastBookElementRef}
       />
     </div>
