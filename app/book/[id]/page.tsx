2e5e0f806391e079cd69db9e51b38fdd050d31d0
Jang <sdsd090811@gmail.com>
2025-02-15 15:17:30 +0900

    refactor: 불필요한 코드 제거

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,22 +1,5 @@
-import Review from "@/components/molecules/Review";
 import { BookDetail } from "@/components/organisms/BookDetail";
-import { fetchBooksPreview } from "@/utils/api/BookPreviewApi";
 import { BookReviewList } from "@/components/organisms/BookReviewList";
-
-async function getData() {
-  const res = await fetch("https://api.example.com/...", { cache: "no-store" });
-  fetchBooksPreview;
-  // ISR 방식에서 revalidate: 0 옵션을 주는 것과 동일하다.
-  // ex) fetch(URL, { next: { revalidate: 0 } });
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 type Params = { id: string };
 
 export default async function BookDetailPage({ params }: { params: Params }) {
