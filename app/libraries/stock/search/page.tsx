e0fbc65442919961632b30279439d8fa55d7790c
Jang <sdsd090811@gmail.com>
2025-03-07 18:02:47 +0900

    feat: 지도 표시 컴포넌트 추가

--- app/libraries/stock/search/page.tsx
+++ app/libraries/stock/search/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import { LibraryMap } from "@/components/organisms/LibraryMap";
 import LibraryPage from "@/components/templates/LibraryPage";
 
 export default function Libraries() {
   return (
     <main className="min-h-screen bg-gray-100">
-      <LibraryPage />
+      {/* <LibraryPage /> */}
+      <LibraryMap />
     </main>
   );
 }
---
+++ components/organisms/LibraryMap.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Map } from "react-kakao-maps-sdk";
+
+const apiKey: string | undefined = process.env.NEXT_PUBLIC_KAKAO_JS;
+const kakaoMapSrc = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&autoload=false`;
+
+export const LibraryMap = () => {
+  const [scriptLoad, setScriptLoad] = useState<boolean>(false);
+
+  useEffect(() => {
+    const script: HTMLScriptElement = kakaoMapScript();
+    document.head.appendChild(script);
+    console.log(script);
+
+    script.addEventListener("load", () => {
+      setScriptLoad(true);
+    });
+  }, []);
+
+  function kakaoMapScript(): HTMLScriptElement {
+    const script: HTMLScriptElement = document.createElement("script");
+    script.src = kakaoMapSrc;
+    script.async = true;
+    return script;
+  }
+  return (
+    <div>
+      {scriptLoad ? (
+        <Map
+          center={{ lat: 33.5563, lng: 126.79581 }}
+          style={{ width: "800px", height: "600px" }}
+          level={3}
+        ></Map>
+      ) : (
+        <div>로딩중</div>
+      )}
+    </div>
+  );
+};
