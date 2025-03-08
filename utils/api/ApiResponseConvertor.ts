9c6f258eca98d2915f97b3c3345a24ef110b5d57
Jang <sdsd090811@gmail.com>
2025-03-08 10:35:55 +0900

    refactor: libraryStock response 변환 코드를 response 변환기로 이동

--- utils/api/ApiResponseConvertor.ts
+++ utils/api/ApiResponseConvertor.ts
@@ -1,4 +1,8 @@
 import { BookPreview } from "@/types/BookPreview";
+import {
+  BookStockStatus,
+  NearbyLibraryStock,
+} from "@/types/NearbyLibraryStock";
 
 export function convertBookPreview(content: any): BookPreview {
   return {
@@ -8,4 +12,25 @@ export function convertBookPreview(content: any): BookPreview {
     publicationYear: content.publicationYear ?? /* undefined */ 1111,
     publisher: content.publisher ?? /* undefined */ "Unknown",
   };
+}
+
+export function convertLibraryStock(content: any): NearbyLibraryStock {
+  return {
+    library: {
+      libraryId: content.library.libraryId,
+      libraryName: content.library.libraryName,
+      distanceMeter: content.library.distanceMeter,
+    },
+    availableBooksCount: content.availableBooksCount,
+    unavailableBooksCount: content.unavailableBooksCount,
+    bookStocks: content.bookStocks.map(convertBookStockStatus),
+  };
+
+  function convertBookStockStatus(content: any): BookStockStatus {
+    return {
+      id: content.id,
+      title: content.title,
+      available: content.available,
+    };
+  }
 }
--- utils/api/LibraryStockSearchApi.ts
+++ utils/api/LibraryStockSearchApi.ts
@@ -1,9 +1,7 @@
-import {
-  BookStockStatus,
-  NearbyLibraryStock,
-} from "@/types/NearbyLibraryStock";
+import { NearbyLibraryStock } from "@/types/NearbyLibraryStock";
 import { get } from "./Fetcher";
 import { Location } from "@/types/Location";
+import { convertLibraryStock } from "./ApiResponseConvertor";
 
 interface Props {
   bookIds: string[];
@@ -16,7 +14,7 @@ export const fetchNearByLibraryStock = async ({
 }: Props): Promise<NearbyLibraryStock[]> => {
   const api: string = createApi({ bookIds, location });
   return get(api).then((content) => {
-    return content.map(convertResponse);
+    return content.map(convertLibraryStock);
   });
 };
 
@@ -28,25 +26,4 @@ function createApi({ bookIds, location }: Props): string {
   url.searchParams.append("latitude", location.latitude.toString());
   url.searchParams.append("longitude", location.longitude.toString());
   return url.toString();
-}
-
-function convertResponse(content: any): NearbyLibraryStock {
-  return {
-    library: {
-      libraryId: content.library.libraryId,
-      libraryName: content.library.libraryName,
-      distanceMeter: content.library.distanceMeter,
-    },
-    availableBooksCount: content.availableBooksCount,
-    unavailableBooksCount: content.unavailableBooksCount,
-    bookStocks: content.bookStocks.map(convertBookStockStatus),
-  };
-}
-
-function convertBookStockStatus(content: any): BookStockStatus {
-  return {
-    id: content.id,
-    title: content.title,
-    available: content.available,
-  };
 }
