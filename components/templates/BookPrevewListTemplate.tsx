a06d6d3d676002889e7ca33910422687b67a2098
Jang <sdsd090811@gmail.com>
2025-03-27 18:59:31 +0900

    refactor: 책 preview를 모바일에서는 2개씩 나오도록 변경

--- components/templates/BookPrevewListTemplate.tsx
+++ components/templates/BookPrevewListTemplate.tsx
@@ -18,7 +18,7 @@ export const BookPreviewList = ({
     <div>
       {!isLoading && searchResults.length === 0 && <EmptySearchResult />}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 md:gap-6">
         {searchResults.length !== 0 && (
           <>{searchResults.map((book) => createBookInfo(book))}</>
         )}
