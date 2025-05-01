fb7a8be767bd7d1208b278c58a910a7db20fc221
Jang <sdsd090811@gmail.com>
2025-05-01 14:46:41 +0900

    feat: api route 기능을 사용해서 책 세부 내용을 검색하는 코드 추가

--- utils/api/BookDetailApi.ts
+++ utils/api/BookDetailApi.ts
@@ -1,5 +1,5 @@
 import { get } from "./Fetcher";
 
 export const fetchBooksDetail = async (bookId: string) => {
-  return get(process.env.NEXT_PUBLIC_API_SERVER_URL + "/api/books/" + bookId);
+  return get(process.env.NEXT_PUBLIC_FRONT_SERVER_URL + "/api/books/" + bookId);
 };
