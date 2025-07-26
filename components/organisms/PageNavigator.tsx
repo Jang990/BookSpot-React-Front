a85dc5ce2ff693100bdc3d97be6ec98d166fb432
Jang <sdsd090811@gmail.com>
2025-07-26 14:43:14 +0900

    feat: 최대 페이지 이후 커서 기반 페이징으로 전환된다는 안내 패널 추가

--- components/organisms/PageNavigator.tsx
+++ components/organisms/PageNavigator.tsx
@@ -4,6 +4,7 @@ import { NumberPageNavigator } from "@/components/molecules/pagination/NumberPag
 import { MAX_NUMBER_PAGE, SearchAfter } from "@/types/Pageable";
 import { useSearchParams } from "next/navigation";
 import { CursorPageNavigator } from "../molecules/pagination/CursorPageNavigator";
+import { InfoPanel } from "../molecules/InfoPanel";
 
 interface PageNaviProps {
   totalPages: number | null;
@@ -11,11 +12,7 @@ interface PageNaviProps {
   hasNext?: boolean;
 }
 
-export const PageNavigator = ({
-  totalPages,
-  searchAfter,
-  hasNext,
-}: PageNaviProps) => {
+export const PageNavigator = ({ totalPages, searchAfter }: PageNaviProps) => {
   const searchParams = useSearchParams();
 
   const pageParam = searchParams.get("page");
@@ -26,20 +23,28 @@ export const PageNavigator = ({
     searchParams.get("lastBookId") !== null;
   const isOutOfPageNumber: boolean = currentPage > MAX_NUMBER_PAGE;
 
+  const hasNext =
+    totalPages !== null && currentPage < totalPages && totalPages !== 0;
+
   return (
     <>
       {hasOnlyCursorCond || isOutOfPageNumber || totalPages == null ? (
         <CursorPageNavigator searchAfter={searchAfter} hasNextPage={true} />
       ) : (
-        <NumberPageNavigator
-          currentPage={currentPage}
-          totalPages={Math.min(totalPages, MAX_NUMBER_PAGE)}
-          hasNext={
-            currentPage === totalPages ||
-            totalPages === 0 ||
-            totalPages > MAX_NUMBER_PAGE
-          }
-        />
+        <div>
+          <NumberPageNavigator
+            currentPage={currentPage}
+            totalPages={Math.min(totalPages, MAX_NUMBER_PAGE)}
+            hasNext={hasNext}
+          />
+          {currentPage === MAX_NUMBER_PAGE && hasNext && (
+            <div className="flex justify-center">
+              <InfoPanel
+                text={`검색 품질을 위해 ${MAX_NUMBER_PAGE} 페이지 이후부터는 번호 선택이 불가능합니다. '다음' 버튼을 이용해 주세요.`}
+              />
+            </div>
+          )}
+        </div>
       )}
     </>
   );
