681f4379c188c25611f165ebedfa0ee5d2959038
Jang <sdsd090811@gmail.com>
2025-07-30 20:35:41 +0900

    fix: useSearchParams로 발생하는 빌드 오류를 suspense를 사용하여 임시 수정

--- app/books/categories/page.tsx
+++ app/books/categories/page.tsx
@@ -1,5 +1,12 @@
 import { BookCategoryPageTemplate } from "@/components/templates/BookCategoryPageTemplate";
+import { Suspense } from "react";
 
 export default function CategorySelector() {
-  return <BookCategoryPageTemplate />;
+  return (
+    <>
+      <Suspense fallback={<div>로딩중…</div>}>
+        <BookCategoryPageTemplate />
+      </Suspense>
+    </>
+  );
 }
