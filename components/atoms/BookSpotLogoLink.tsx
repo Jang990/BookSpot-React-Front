e02d619123bf92e87137e1d4af6382b40063424a
Jang <sdsd090811@gmail.com>
2025-08-10 12:15:49 +0900

    feat: 헤더의 링크를 Link대신 a 태그를 사용하도록 변경

--- components/atoms/BookSpotLogoLink.tsx
+++ components/atoms/BookSpotLogoLink.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export const BookSpotLogoButton = () => {
   return (
-    <Link href="/" className="text-2xl font-bold text-primary">
+    <a href="/" className="text-2xl font-bold text-primary">
       BookSpot
-    </Link>
+    </a>
   );
 };
