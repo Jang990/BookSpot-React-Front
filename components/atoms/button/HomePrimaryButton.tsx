2be404b30a13b7425fd3734b80806df3716a47db
Jang <sdsd090811@gmail.com>
2025-08-10 11:49:09 +0900

    style: 홈으로 돌아가기 -> 홈으로 아이콘 버튼으로 변경

--- components/atoms/button/HomePrimaryButton.tsx
+++ components/atoms/button/HomePrimaryButton.tsx
@@ -1,11 +1,13 @@
+import { Home } from "lucide-react";
+
 export const HomePrimaryButton = () => {
   return (
     <>
       <a
         href="/"
-        className="mt-6 inline-block rounded-md bg-primary px-6 py-3 text-white hover:bg-primary-dark transition"
+        className="mt-6 inline-flex items-center gap-2 rounded-md bg-primary px-6 py-3 text-white hover:bg-primary-dark transition"
       >
-        홈으로 돌아가기
+        <Home /> 홈으로
       </a>
     </>
   );
