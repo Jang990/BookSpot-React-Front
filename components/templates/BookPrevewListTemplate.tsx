68af3df1d0ea9de3e683329de30b88ebe0509267
Jang <sdsd090811@gmail.com>
2025-08-12 21:05:40 +0900

    feat: 북카트에 책 추가시 추가/제한 정보 토스트 추가

--- components/templates/BookPrevewListTemplate.tsx
+++ components/templates/BookPrevewListTemplate.tsx
@@ -1,12 +1,39 @@
+"use client";
 import { BookPreview } from "@/types/BookPreview";
 import { EmptySearchResult } from "../molecules/EmptySearchResult";
 import { SearchableBookInfo } from "../organisms/book/preview/SearchableBookInfo";
+import { useBookCart } from "@/contexts/BookCartContext";
+import { useState } from "react";
+import { InfoToast } from "../molecules/toast/InfoToast";
 
 interface BookPreviewListProps {
   searchResults: BookPreview[];
 }
 
 export const BookPreviewList = ({ searchResults }: BookPreviewListProps) => {
+  const { addToCart } = useBookCart();
+  const [toast, setToast] = useState<{
+    message: string;
+    type: "INFO" | "WARN";
+  } | null>(null);
+
+  const showToast = (message: string, type: "INFO" | "WARN" = "INFO") => {
+    setToast({ message, type });
+  };
+
+  const handleAddToCart = (bookId: string) => {
+    try {
+      addToCart(bookId);
+      showToast("장바구니에 추가되었습니다.", "INFO");
+    } catch (err) {
+      if (err instanceof Error) {
+        showToast(err.message, "WARN");
+      } else {
+        showToast("알 수 없는 오류가 발생했습니다.", "WARN");
+      }
+    }
+  };
+
   return (
     <div>
       {searchResults.length === 0 && <EmptySearchResult />}
@@ -18,15 +45,20 @@ export const BookPreviewList = ({ searchResults }: BookPreviewListProps) => {
               <SearchableBookInfo
                 key={book.id}
                 book={book}
+                onClickAddBtn={handleAddToCart}
               ></SearchableBookInfo>
             ))}
           </>
         )}
       </div>
 
-      {/* <div className="flex justify-center items-center mt-8">
-        {isLoading && <Loading />}
-      </div> */}
+      {toast && (
+        <InfoToast
+          message={toast.message}
+          type={toast.type}
+          onClose={() => setToast(null)}
+        />
+      )}
     </div>
   );
 };
--- utils/BookCartLocalStorage.ts
+++ utils/BookCartLocalStorage.ts
@@ -9,7 +9,9 @@ export function findBookIds(): string[] {
 
 export function addBookId(bookId: string): boolean {
   const cart = findBookIds();
-  if (cart.includes(bookId) || cart.length > MAX_CART_SIZE) return false;
+  if (cart.length === MAX_CART_SIZE) throw new Error("북카트가 가득 찼습니다.");
+  if (cart.includes(bookId))
+    throw new Error("북카트에 이미 존재하는 책입니다.");
 
   cart.push(bookId);
   save(cart);
