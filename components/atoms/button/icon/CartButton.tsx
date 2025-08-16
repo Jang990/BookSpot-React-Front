571641c8594a457775f6e14360002fccf3c2cd81
Jang <sdsd090811@gmail.com>
2025-08-16 15:44:55 +0900

    feat: 책 정보에 등장하는 버튼들에 설명 title 속성 추가

--- components/atoms/button/icon/CartButton.tsx
+++ components/atoms/button/icon/CartButton.tsx
@@ -12,6 +12,7 @@ export const CartButton = ({ onClick }: BookCartProps) => {
         onClick();
       }}
       className="bg-primary text-primary-foreground p-1.5 rounded-full hover:bg-primary/80 transition-colors animate-in zoom-in-50 duration-200 active:scale-90"
+      title="북카트에 담기"
     >
       <ShoppingCart size={20} />
     </button>
