f230a4b121176b2d7129a0256a036ab451505b00
Jang <sdsd090811@gmail.com>
2025-08-16 16:07:36 +0900

    feat: html 언어를 ko로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
         <BookCartProvider>
           <div className="flex flex-col min-h-screen">
