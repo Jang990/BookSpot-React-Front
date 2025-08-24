fce7fc03403c889fa044cba79993b4ce330f03a7
Jang <sdsd090811@gmail.com>
2025-08-24 14:27:59 +0900

    feat: BookRankings에 enum을 화면에 표시할 text로 변환하는 로직 추가

--- types/BookRankings.ts
+++ types/BookRankings.ts
@@ -19,6 +19,45 @@ export enum RankingAge {
   AGE_50_UP = "AGE_50_UP",
 }
 
+export function textAge(age: RankingAge): string {
+  switch (age) {
+    case RankingAge.ALL:
+      return "ALL";
+    case RankingAge.AGE_0_14:
+      return "어린이";
+    case RankingAge.AGE_15_19:
+      return "청소년";
+    case RankingAge.AGE_20_29:
+      return "20대";
+    case RankingAge.AGE_30_39:
+      return "30대";
+    case RankingAge.AGE_40_49:
+      return "40대";
+    case RankingAge.AGE_50_UP:
+      return "50대 이상";
+  }
+}
+
+export function textGender(gender: RankingGender): string {
+  switch (gender) {
+    case RankingGender.ALL:
+      return "전체";
+    case RankingGender.MALE:
+      return "남성";
+    case RankingGender.FEMALE:
+      return "여성";
+  }
+}
+
+export function textPeriod(gender: RankingPeriod): string {
+  switch (gender) {
+    case RankingPeriod.MONTHLY:
+      return "월간";
+    case RankingPeriod.WEEKLY:
+      return "주간";
+  }
+}
+
 export type RankingConditions = {
   period: RankingPeriod;
   gender: RankingGender;
