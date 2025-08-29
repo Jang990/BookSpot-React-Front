af6aa80205302538fa72afc86eba91721bbeb2a6
Jang <sdsd090811@gmail.com>
2025-08-29 15:49:28 +0900

    feat: 대출과 관련 된 인터페이스 및 타입 선언

---
+++ types/Loan.ts
@@ -0,0 +1,17 @@
+export interface LibraryBookStockInfo {
+  bookTitle: string;
+  bookAuthor: string;
+  bookPublicationYear: string;
+  isInLibrary: boolean;
+  loanInfo: LoanInfo | null;
+}
+
+export type LoanState = "LOANABLE" | "ON_LOAN" | "UNKNOWN" | "ERROR";
+
+export interface LoanInfo {
+  stockId: string;
+  libraryId: string;
+  bookId: string;
+  loanState: LoanState;
+  updatedAt: string;
+}
