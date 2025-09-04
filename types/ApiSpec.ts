9b0d9fe81b40002dc5331ca8e7d464757978407d
Jang <sdsd090811@gmail.com>
2025-09-04 14:38:42 +0900

    feat: 책 SearchAfter 쿼리 api 스펙 추가

--- types/ApiSpec.ts
+++ types/ApiSpec.ts
@@ -1,10 +1,17 @@
 export interface BookPagingApiSpec {
   books?: {
     content: any[];
-    // [key: string]: any;
     totalElements: number;
   };
   lastScore?: string;
   lastLoanCount?: number;
   lastBookId?: number;
+}
+
+export interface BookSearchAfterApiSpec {
+  books: any[];
+  lastScore?: string;
+  lastLoanCount?: number;
+  lastBookId?: number;
+  totalElements: number;
 }
