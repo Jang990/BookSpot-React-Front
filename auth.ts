6e3be736778868018e4489cc694b6564ca3a44f2
Jang <sdsd090811@gmail.com>
2025-09-05 11:58:45 +0900

    feat: auth 전략을 jwt로 명시

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  session: { strategy: "jwt" },
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
