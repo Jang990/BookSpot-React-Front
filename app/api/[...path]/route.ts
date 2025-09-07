01ac089f6304f0ec2533972a3d120461b6219f13
Jang <sdsd090811@gmail.com>
2025-09-07 15:23:00 +0900

    feat: 통합 백엔드 요청을 담당할 api 추가

---
+++ app/api/[...path]/route.ts
@@ -0,0 +1,18 @@
+import apiClient from "@/utils/api/common/ApiClient";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  return await apiClient.get(req);
+};
+
+export const POST = async (req: NextRequest) => {
+  return await apiClient.post(req);
+};
+
+export const PUT = async (req: NextRequest) => {
+  return await apiClient.put(req);
+};
+
+export const DELETE = async (req: NextRequest) => {
+  return await apiClient.delete(req);
+};
