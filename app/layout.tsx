793e3225694ad75da053854967e30184080f4230
Jang <sdsd090811@gmail.com>
2025-09-16 18:11:21 +0900

    style: 주석 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", type: "image/x-icon" }, // fallback
+      { url: "/favicon.ico", type: "image/x-icon" },
     ],
     apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
     other: [
