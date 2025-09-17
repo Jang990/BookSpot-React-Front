ee6b543f9e0eaa0ad4421e3a287c89662fd4888e
Jang <sdsd090811@gmail.com>
2025-09-17 15:27:22 +0900

    feat: 임시 페이지가 아닌 어느 페이지에서든 처음 사용하는 사용자라면 보여주도록 변경

--- app/temp/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnboardingModal } from "@/components/organisms/onboarding/OnBoardingModal";
-
-export default function TempPage() {
-  return (
-    <div>
-      <OnboardingModal />
-    </div>
-  );
-}
