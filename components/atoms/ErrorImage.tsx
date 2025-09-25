0d3de2862d81494b7bbb9be9c70f22ddafe66177
Jang <sdsd090811@gmail.com>
2025-09-25 16:41:15 +0900

    style: PC 모니터에서 에러 이미지가 너무 크지 않도록 클래스 변경

--- components/atoms/ErrorImage.tsx
+++ components/atoms/ErrorImage.tsx
@@ -7,7 +7,7 @@ export const ErrorImage = ({ src, alt }: Props) => {
     <img
       src={src}
       alt={alt}
-      className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+      className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-md"
       style={{ aspectRatio: "1 / 1" }}
     />
   );
