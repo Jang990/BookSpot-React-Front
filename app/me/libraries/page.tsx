cc2dad8e848ca6dcbff0f60a0a114b5cfe5b4cd9
Jang <sdsd090811@gmail.com>
2025-09-25 16:40:48 +0900

    refactor: 404 notFound 페이지의 공통 컴포넌트를 분리

--- app/me/libraries/page.tsx
+++ app/me/libraries/page.tsx
@@ -1,18 +1,14 @@
 import { HomePrimaryButton } from "@/components/atoms/button/HomePrimaryButton";
 import { ErrorImage } from "@/components/atoms/ErrorImage";
+import { Error404Page } from "@/components/molecules/Error404Page";
 
 export default async function MyLibraries() {
   return (
-    <div className="flex flex-col items-center justify-center flex-grow text-center">
-      <ErrorImage src="/404.svg" alt="404" />
-      <div>
-        <h1 className="mt-2 text-2xl font-bold text-gray-800">
-          개발 중인 페이지에요. 9월 안으로 추가될 수 있게 노력할게요.
-        </h1>
-        <div className="mt-4 flex justify-center gap-4">
-          <HomePrimaryButton />
-        </div>
-      </div>
-    </div>
+    <Error404Page
+      messages={[
+        "개발 중인 페이지에요.",
+        "9월 안으로 추가될 수 있게 노력할게요.",
+      ]}
+    />
   );
 }
