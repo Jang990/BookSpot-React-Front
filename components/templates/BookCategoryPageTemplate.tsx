bd2743ffa40ce470a7080f26a60545c7041191f7
Jang <sdsd090811@gmail.com>
2025-09-25 20:16:55 +0900

    feat: 카테고리 선택 시 searchTerm이 유지되도록 수정

--- components/templates/BookCategoryPageTemplate.tsx
+++ components/templates/BookCategoryPageTemplate.tsx
@@ -34,7 +34,7 @@ const queryString = (
   params.set(CATEGORY_QUERY_STRING_KEY, String(categoryId));
   params.set(CATEGORY_LEVEL_QUERY_STRING_KEY, categoryLevel);
   params.delete(CATEGORY_HISTORY_QUERY_STRING_KEY);
-  params.delete(SEARCH_TERM_KEY);
+  // params.delete(SEARCH_TERM_KEY);
   return params.toString();
 };
 
@@ -45,7 +45,7 @@ export const onClickCategory = (
   categoryLevel: string,
   clearSearchTerm: () => void
 ) => {
-  clearSearchTerm();
+  // clearSearchTerm();
   router.push(
     `/books/?${queryString(searchParams, categoryId, categoryLevel)}`
   );
