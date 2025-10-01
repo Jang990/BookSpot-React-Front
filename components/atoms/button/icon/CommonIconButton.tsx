7cbbc433825b2ea0438cd95fc55b40e7569f7d87
Jang <sdsd090811@gmail.com>
2025-10-01 17:30:00 +0900

    feat: 드롭박스를 보여주는 아이콘 버튼 추가

--- components/atoms/button/icon/CommonIconButton.tsx
+++ components/atoms/button/icon/CommonIconButton.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import React from "react";
 
 export const CommonIconButton = ({
@@ -15,5 +21,42 @@ export const CommonIconButton = ({
     <Button variant="ghost" size="sm" onClick={onClick} disabled={disabled}>
       {icon}
     </Button>
+  );
+};
+
+export type DropdownItem = {
+  icon: React.ReactNode;
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
+};
+
+interface IconDropdown {
+  icon: React.ReactNode;
+  items: DropdownItem[];
+}
+
+export const CommonIconDropdown = ({ icon, items }: IconDropdown) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="sm">
+          {icon}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {items.map((item, index) => (
+          <DropdownMenuItem
+            key={index}
+            onClick={item.onClick}
+            disabled={item.disabled}
+            className="flex items-center gap-2 cursor-pointer"
+          >
+            {item.icon}
+            <span>{item.label}</span>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
