ea6f7cd45a30ae75cf3c0526b4c4f183a4d92c3c
Jang <sdsd090811@gmail.com>
2025-10-29 08:38:23 +0900

    fix: 페이지 동적 생성으로 빌드 오류 해결

--- app/bookshelves/page.tsx
+++ app/bookshelves/page.tsx
@@ -1,6 +1,8 @@
 import { PublicBookshelvesListTemplate } from "@/components/templates/PublicBookshelvesListTemplate";
 import { fetchPublicBookShelves } from "@/utils/api/BookshelfApi";
 
+export const dynamic = "force-dynamic";
+
 export default async function PublicBookShelvesListPage() {
   const shelves = (
     await fetchPublicBookShelves({
